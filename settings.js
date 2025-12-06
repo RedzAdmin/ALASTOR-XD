3591be0a890c7587dea32f997db8e8d8c75b89cf
C0D3-BR34K3R001 <mbanemew9@gmail.com>
2025-12-06 09:05:37 +0100

    Initial commit

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'ALASTOR-XD',
+  author: 'CODEBREAKER',
+  botName: "ALASTOR-XD",
+  botOwner: 'CODEBREAKER', // Your name
+  ownerNumber: '2347030626048', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'NrSjG6var2uiuSYDm0xTqCX0xcFgGj4s',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.5",
+  updateZipUrl: "https://github.com/RedzAdmin/ALASTOR-XD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
